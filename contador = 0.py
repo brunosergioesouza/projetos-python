d2f21c38e6831b7c525152af2ceb95c68ff94f90
Bruno Sergio <brunosergioesouza@gmail.com>
2026-05-24 11:18:49 -0300

    primeiro commit

---
+++ contador = 0.py
@@ -0,0 +1,22 @@
+contador = 10
+
+print('Faltam apenas 10 segundos.')
+
+while contador > 0:
+    contador -= 1
+    if contador % 2 == 0:
+        print(f'Faltam apenas {contador} segundos - Não perca essa oportunidade!')
+    else:
+        print(f'A contagem continua: {contador} segundos restantes')
+    
+
+print('Aproveite a promoção agora!')
+
+        
+        
+
+
+
+    
+    
+    
